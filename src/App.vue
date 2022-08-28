90cf6db6dd76e28a361fdd2e1e544acc66440c11
smj187 <engulf_clamshell_joining_repulsion@proton.me>
2022-08-28 20:06:39 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+  import Header from "@/components/Header.vue"
+  import { RouterView } from "vue-router"
+</script>
+
+<template>
+  <div class="relative">
+    <Header />
+    <main class="mt-16 p-9">
+      <RouterView />
+    </main>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .container {
+    min-height: 100vh;
+    background-color: #141414;
+    color: #e8e8e8;
+  }
+</style>
